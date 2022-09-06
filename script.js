55f71d4ceae7b23d8392b27d760dd366342323ea
Abit Salihu <abiti.salihu@gmail.com>
2022-09-06 14:46:48 +0200

    add the UI of the thank you card, and submit button behaviour in js.

--- script.js
+++ script.js
@@ -2,30 +2,41 @@
 
 //? HTML Elements
 
-const buttonsCon = document.querySelector(".rating__buttons");
-const fButton = document.querySelector(".first");
-const sButton = document.querySelector(".second");
-const thButton = document.querySelector(".third");
-const foButton = document.querySelector(".fourth");
-const fiButton = document.querySelector(".fifth");
+const ratingCon = document.querySelector(".container__rating");
+const thankyouCon = document.querySelector(".container__thankyou");
+const btnsCon = document.querySelector(".container__rating--buttons");
+const fBtn = document.querySelector(".first");
+const sBtn = document.querySelector(".second");
+const thBtn = document.querySelector(".third");
+const foBtn = document.querySelector(".fourth");
+const fiBtn = document.querySelector(".fifth");
+
+const submitBtn = document.querySelector(".container__rating--submit");
+const userMsg = document.querySelector(".container__thankyou--userselection");
 
 //? Starting funciton
 let rating;
 
 //? Function
 const removeClass = function (e) {
-  fButton.classList.remove("active");
-  sButton.classList.remove("active");
-  thButton.classList.remove("active");
-  foButton.classList.remove("active");
-  fiButton.classList.remove("active");
-  e.classList.add("active");
+  fBtn.classList.remove("active-btn");
+  sBtn.classList.remove("active-btn");
+  thBtn.classList.remove("active-btn");
+  foBtn.classList.remove("active-btn");
+  fiBtn.classList.remove("active-btn");
+  e.classList.add("active-btn");
 };
 
-//?
-console.log(rating);
-buttonsCon.addEventListener("click", function (e) {
+//? Event Listeners
+btnsCon.addEventListener("click", function (e) {
   rating = e.target.getAttribute("data-set");
   if (!rating) return;
   removeClass(e.target);
+});
+
+submitBtn.addEventListener("click", () => {
+  if (!rating) return;
+  userMsg.textContent = `You selected ${rating} out of 5`;
+  ratingCon.classList.add("hidden");
+  thankyouCon.classList.remove("hidden");
 });
